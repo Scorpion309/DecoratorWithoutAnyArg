18a4c782bbe5b8e88edc242ea466b4a7500e70d9
Scorpion309 <Tustin@tut.by>
2021-06-04 20:37:58 +0300

    del sugar

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ def get_args_for_decorator(start):
 
     return decorator
 
+def args_without_first_arg(args_from_decorator):
+    print(args_from_decorator)
 
 if __name__ == '__main__':
-    @get_args_for_decorator(3)
-    def args_without_first_arg(args_from_decorator):
-        print(args_from_decorator)
+    args_without_first_arg = get_args_for_decorator(3)(args_without_first_arg)
+
 
 
     args_without_first_arg(1, 2, 3, 4, 5, 6)
